140161365d4e90627209bbdcecb84615c0e12941
Fabio de Matos <fdematos@despegar.com>
2018-08-20 14:01:13 +1000

    Initial commit

---
+++ app/src/main/java/com/example/fmatosqg/sample/imgurlight/LandingActivity.kt
@@ -0,0 +1,12 @@
+package com.example.fmatosqg.sample.imgurlight
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LandingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.landing_activity)
+    }
+}
